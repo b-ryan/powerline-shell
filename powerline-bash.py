87dc91527d54d22a4c3b5f04a24df0feb6490da1
Shrey Banga <banga.shrey@gmail.com>
2012-09-13 18:42:54 -0400

    Increase max path length before shortening

--- powerline-bash.py
+++ powerline-bash.py
@@ -175,7 +175,7 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     #p.append(Segment(' \\u ', 250, 240))
     #p.append(Segment(' \\h ', 250, 238))
-    add_cwd_segment(p, cwd, 4)
+    add_cwd_segment(p, cwd, 5)
     add_repo_segment(p, cwd)
     add_root_indicator(p, sys.argv[1] if len(sys.argv) > 1 else 0)
     sys.stdout.write(p.draw())
