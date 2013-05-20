b3f09ea8e1ac1f6daf6da6dc05462e6b25a492f8
lhl <lhl@randomfoo.net>
2013-05-19 23:51:35 -0700

    Added 'bare' options for hostname and username (required for fish compatibility)

--- segments/username.py
+++ segments/username.py
@@ -1,7 +1,10 @@
+import os
+
 def add_username_segment():
     user_prompts = {
         'bash': ' \\u',
-        'zsh': ' %n'
+        'zsh': ' %n',
+        'bare': ' %s' % os.getenv('USER')
     }
     powerline.append(user_prompts[powerline.args.shell], Color.USERNAME_FG,
             Color.USERNAME_BG)
