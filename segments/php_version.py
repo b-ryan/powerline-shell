7afba0fdf47d95a070816d3340c5731fb2f5e1cb
Márcio Almada <marcio3w@gmail.com>
2013-06-16 17:35:51 -0300

    Improving php_version segment: better way to get version number

--- segments/php_version.py
+++ segments/php_version.py
@@ -3,7 +3,7 @@ import subprocess
 
 def add_php_version_segment():
     try:
-        version = ' %s ' % subprocess.check_output(['php', '--version'], stderr=subprocess.STDOUT).split(' ')[1]
+        version = ' %s ' % subprocess.check_output(['php', '-r', 'echo PHP_VERSION;'], stderr=subprocess.STDOUT)
         powerline.append(version, 15, 4)
     except OSError:
         return
