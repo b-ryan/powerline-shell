df4dc2e443949f202dca4922930e04ae7b694478
Márcio Almada <marcio3w@gmail.com>
2013-06-17 10:58:14 -0300

    Fixed php_version segment: wrong php version number when running on Ubuntu with system wide PHP installation

--- segments/php_version.py
+++ segments/php_version.py
@@ -3,7 +3,12 @@ import subprocess
 
 def add_php_version_segment():
     try:
-        version = ' %s ' % subprocess.check_output(['php', '-r', 'echo PHP_VERSION;'], stderr=subprocess.STDOUT)
+        output = subprocess.check_output(['php', '-r', 'echo PHP_VERSION;'], stderr=subprocess.STDOUT)
+        if '-' in output:
+            version = ' %s ' % output.split('-')[0]
+        else:
+            version = ' %s ' % output
+
         powerline.append(version, 15, 4)
     except OSError:
         return
