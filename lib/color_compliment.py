7e289bedcc5e9c5966a0ef7d200ea04091fc5787
H. Ryan Jones <hryanjones@gmail.com>
2013-07-20 06:46:50 -0700

    Tweaking of opposite color picker, also fixed a bug causing RGB values to go over 255

--- lib/color_compliment.py
+++ lib/color_compliment.py
@@ -8,15 +8,23 @@ from sys import argv
 
 def getOppositeColor(r,g,b):
     hls = rgb_to_hls(r,g,b)
+    #print "hls is"
+    #print hls
     opp = list(hls[:])
-    opp[0] = 255-opp[0] # reverse hue (a.k.a. color)
+    #opp[0] = (opp[0]+0.5)%1 # reverse hue (a.k.a. color), reversing tends to be jarring
+    opp[0] = (opp[0]+0.2)%1 # shift hue (a.k.a. color)
     if opp[1] > 255/2:   # for level you want to make sure they
         opp[1] -= 255/2  # are quite different so easily readable
     else:
         opp[1] += 255/2
-    opp[1], 255-opp[2]
-    opp = ( int(x) for x in opp)
-    return hls_to_rgb(*opp)
+    if opp[2] > -0.5: # if saturation is low on first color increase second's
+        opp[2] -= 0.5
+    #print opp
+    opp = hls_to_rgb(*opp)
+    m = max(opp)
+    if m > 255: #colorsys module doesn't give caps to their conversions
+        opp = [ x*254/m for x in opp]
+    return tuple([ int(x) for x in opp])
 
 def stringToHashToColorAndOpposite(string):
     string = md5(string).hexdigest()[:6] # get a random color
