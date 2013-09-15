b50642791293d65007afc96f68a2efba332aff9d
Márcio Almada <marcio3w@gmail.com>
2013-09-15 20:47:17 -0300

    keeping spaces consistent across different terminals

--- segments/hostname.py
+++ segments/hostname.py
@@ -13,10 +13,10 @@ def add_hostname_segment():
         if powerline.args.shell == 'bash':
             host_prompt = ' \\h '
         elif powerline.args.shell == 'zsh':
-            host_prompt = ' %m'
+            host_prompt = ' %m '
         else:
             import socket
-            host_prompt = ' %s' % socket.gethostname().split('.')[0]
+            host_prompt = ' %s ' % socket.gethostname().split('.')[0]
 
         powerline.append(host_prompt, Color.HOSTNAME_FG, Color.HOSTNAME_BG)
 
--- segments/username.py
+++ segments/username.py
@@ -3,10 +3,10 @@ def add_username_segment():
     if powerline.args.shell == 'bash':
         user_prompt = ' \\u '
     elif powerline.args.shell == 'zsh':
-        user_prompt = ' %n'
+        user_prompt = ' %n '
     else:
         import os
-        user_prompt = ' %s' % os.getenv('USER')
+        user_prompt = ' %s ' % os.getenv('USER')
 
     powerline.append(user_prompt, Color.USERNAME_FG, Color.USERNAME_BG)
 
