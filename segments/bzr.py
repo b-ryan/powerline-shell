dc99f25f761efe41bd568e8ebbfee7757580c57f
= <=>
2014-02-11 12:17:49 +0100

    Avoid errors when no bzrlib, better loop

--- segments/bzr.py
+++ segments/bzr.py
@@ -1,10 +1,18 @@
-from bzrlib import bzrdir, urlutils, workingtree, status, errors
-from StringIO import StringIO
+try:
+    from bzrliba import bzrdir, urlutils, workingtree, status, errors
+    add_bzr_segment = bzr_segment
+except ImportError:
+    add_bzr_segment = lambda: False
+
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
 import os
 
 COLOCATED_LOCATION = '.bzr/branches'
 
-def add_bzr_segment():
+def bzr_segment():
     location = os.getcwd()
 
     try:
@@ -31,18 +39,18 @@ def add_bzr_segment():
 
     has_modified_files, has_untracked_files, has_missing_files = False, False, False
 
-    tof = StringIO()
-    status.show_tree_status(working, short=True, to_file=tof)
-    tof.seek(0)
-    values = tof.readlines()
-
     if working:
-        for line in values:
-            if line.split()[0] == 'M':
+        tof = StringIO()
+        status.show_tree_status(working, short=True, to_file=tof)
+        tof.seek(0)
+        statuses = set(l.strip()[0] for l in tof.readlines())
+
+        for status in statuses:
+            if status == 'M':
                 has_modified_files = True
-            elif line.split()[0] == '?':
+            elif status == '?':
                 has_untracked_files = True
-            elif line.split()[0] == 'D':
+            elif status == 'D':
                 has_missing_files = True
 
     if has_modified_files or has_missing_files:
