f6a3a9ade5873477bafa04a42283cb06a95ad8fc
Christopher Welborn <cj@welbornprod.com>
2014-07-12 20:48:00 -0500

    changed cwd display to parent and last N children

--- segments/cwd.py
+++ segments/cwd.py
@@ -1,17 +1,20 @@
 import os
 
+
 def get_short_path(cwd):
     home = os.getenv('HOME')
     names = cwd.split(os.sep)
-    if names[0] == '': names = names[1:]
+    if names[0] == '':
+        names = names[1:]
     path = ''
     for i in range(len(names)):
         path += os.sep + names[i]
         if os.path.samefile(path, home):
-            return ['~'] + names[i+1:]
+            return ['~'] + names[i + 1:]
     if not names[0]:
         return ['/']
     return names
+
 
 def add_cwd_segment():
     cwd = powerline.cwd or os.getenv('PWD')
@@ -19,7 +22,7 @@ def add_cwd_segment():
 
     max_depth = powerline.args.cwd_max_depth
     if len(names) > max_depth:
-        names = names[:2] + [u'\u2026'] + names[2 - max_depth:]
+        names = names[:1] + [u'\u2026'] + names[1 - max_depth:]
 
     if not powerline.args.cwd_only:
         for n in names[:-1]:
@@ -27,7 +30,7 @@ def add_cwd_segment():
                 powerline.append(' %s ' % n, Color.HOME_FG, Color.HOME_BG)
             else:
                 powerline.append(' %s ' % n, Color.PATH_FG, Color.PATH_BG,
-                    powerline.separator_thin, Color.SEPARATOR_FG)
+                                 powerline.separator_thin, Color.SEPARATOR_FG)
 
     if names[-1] == '~' and Color.HOME_SPECIAL_DISPLAY:
         powerline.append(' %s ' % names[-1], Color.HOME_FG, Color.HOME_BG)
