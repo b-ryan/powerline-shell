225d3791c0fc6dfc11439d09d34bc163019dfc73
Dennis Lutter <dennis.lutter@project-collins.com>
2014-08-27 10:24:37 +0200

    clean imports (only one of each) and add segment comments

--- install.py
+++ install.py
@@ -17,18 +17,42 @@ THEMES_DIR = 'themes'
 
 def load_source(srcfile):
     try:
-        return ''.join(open(srcfile).readlines()) + '\n\n'
+        return open(srcfile).readlines()
     except IOError:
         print 'Could not open', srcfile
         return ''
 
+def clean_imports(source):
+    """ removes all additional module level imports"""
+    previous_imports = []
+    clean_lines = []
+    for line in source:
+        if line in previous_imports:
+            continue
+        if line.startswith("import") or line.startswith("from "):
+            previous_imports.append(line)
+        clean_lines.append(line)
+    return clean_lines
+
+def segment_line(name):
+    return '\n#{:#^80}\n'.format(' {} '.format(name))
+
 if __name__ == "__main__":
     source = load_source(TEMPLATE_FILE)
+    source.append(segment_line('defaults'))
     source += load_source(os.path.join(THEMES_DIR, 'default.py'))
+    source.append(segment_line('theme: ' + config.THEME))
     source += load_source(os.path.join(THEMES_DIR, config.THEME + '.py'))
+    source.append(segment_line('SEGMENTS'))
     for segment in config.SEGMENTS:
+        source.append(segment_line(segment))
         source += load_source(os.path.join(SEGMENTS_DIR, segment + '.py'))
-    source += 'sys.stdout.write(powerline.draw())\n'
+
+    source.append(segment_line('write out'))
+    source.append('sys.stdout.write(powerline.draw())\n')
+    source = clean_imports(source)
+    source = ''.join(source) + '\n\n'
+
 
     try:
         open(OUTPUT_FILE, 'w').write(source)
