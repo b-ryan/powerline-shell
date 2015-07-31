bb0bef32bf2456d44c404439965d60419ba3ce59
Buck Ryan <buck.ryan@gmail.com>
2015-07-31 00:06:21 -0400

    change uptime char to unicode byte point and add newline

--- segments/uptime.py
+++ segments/uptime.py
@@ -18,7 +18,7 @@ def add_uptime_segment():
         hour_search =  re.search('\d{1,2}(?=\:)', raw_uptime)
         hours = '' if not hour_search else '%sh ' %  hour_search.group(0)
         minutes =  re.search('(?<=\:)\d{1,2}|\d{1,2}(?=\s+min)', raw_uptime).group(0)
-        uptime = u' %s%s%sm ↑ ' % (days, hours, minutes)
+        uptime = u' %s%s%sm \u2191 ' % (days, hours, minutes)
         powerline.append(uptime, Color.CWD_FG, Color.PATH_BG)
     except OSError:
         return
