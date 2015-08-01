4587f0e958f31970d29558c99ab64d6f7d1a3be0
Buck Ryan <buck.ryan@gmail.com>
2015-07-31 21:52:55 -0400

    rename function in exit_code segment to match filename

--- segments/exit_code.py
+++ segments/exit_code.py
@@ -1,8 +1,8 @@
-def add_status_indicator_segment():
+def add_exit_code_segment():
     if powerline.args.prev_error == 0:
         return
     fg = Color.CMD_FAILED_FG
     bg = Color.CMD_FAILED_BG
     powerline.append(str(powerline.args.prev_error), fg, bg)
 
-add_status_indicator_segment()
+add_exit_code_segment()
