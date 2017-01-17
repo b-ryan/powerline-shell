5a4a6856984396f0273852239de17b2ac0c72f9f
Srinivas Devaki <mr.eightnoteight@gmail.com>
2017-01-17 23:46:29 +0530

    CONDA_ENV_PATH is changed to CONDA_DEFAULT_ENV
    conda/conda#2312

--- segments/virtual_env.py
+++ segments/virtual_env.py
@@ -1,7 +1,7 @@
 import os
 
 def add_virtual_env_segment(powerline):
-    env = os.getenv('VIRTUAL_ENV') or os.getenv('CONDA_ENV_PATH')
+    env = os.getenv('VIRTUAL_ENV') or os.getenv('CONDA_ENV_PATH') or os.getenv('CONDA_DEFAULT_ENV')
     if env is None:
         return
 
