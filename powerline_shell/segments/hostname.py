c3506d075e21171f2cf3abefb933f6bae44e2afa
Buck Ryan <buck.ryan2@gmail.com>
2017-09-05 10:31:10 -0400

    tests for hostname segment

--- powerline_shell/segments/hostname.py
+++ powerline_shell/segments/hostname.py
@@ -11,18 +11,15 @@ class Segment(BasicSegment):
             hostname = gethostname()
             FG, BG = stringToHashToColorAndOpposite(hostname)
             FG, BG = (rgb2short(*color) for color in [FG, BG])
-            host_prompt = ' %s ' % hostname.split('.')[0]
-
+            host_prompt = " %s " % hostname.split(".")[0]
             powerline.append(host_prompt, FG, BG)
         else:
-            if powerline.args.shell == 'bash':
-                host_prompt = ' \\h '
-            elif powerline.args.shell == 'zsh':
-                host_prompt = ' %m '
+            if powerline.args.shell == "bash":
+                host_prompt = r" \h "
+            elif powerline.args.shell == "zsh":
+                host_prompt = " %m "
             else:
-                import socket
-                host_prompt = ' %s ' % socket.gethostname().split('.')[0]
-
+                host_prompt = " %s " % gethostname().split(".")[0]
             powerline.append(host_prompt,
                              powerline.theme.HOSTNAME_FG,
                              powerline.theme.HOSTNAME_BG)
