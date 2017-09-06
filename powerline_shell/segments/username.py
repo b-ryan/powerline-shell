d23f3943502044e8088b68b30ef4a0602efc1d38
Buck Ryan <buck.ryan2@gmail.com>
2017-09-05 21:47:03 -0400

    file formatting for username segment

--- powerline_shell/segments/username.py
+++ powerline_shell/segments/username.py
@@ -1,18 +1,18 @@
 from ..utils import BasicSegment
+import os
 
 
 class Segment(BasicSegment):
     def add_to_powerline(self):
         powerline = self.powerline
-        import os
-        if powerline.args.shell == 'bash':
-            user_prompt = ' \\u '
-        elif powerline.args.shell == 'zsh':
-            user_prompt = ' %n '
+        if powerline.args.shell == "bash":
+            user_prompt = r" \u "
+        elif powerline.args.shell == "zsh":
+            user_prompt = " %n "
         else:
-            user_prompt = ' %s ' % os.getenv('USER')
+            user_prompt = " %s " % os.getenv("USER")
 
-        if os.getenv('USER') == 'root':
+        if os.getenv("USER") == "root":
             bgcolor = powerline.theme.USERNAME_ROOT_BG
         else:
             bgcolor = powerline.theme.USERNAME_BG
