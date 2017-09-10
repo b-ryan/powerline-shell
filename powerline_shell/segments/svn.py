44a7f2c3e0d85e556a95c9444958e33d4701e9b0
Buck Ryan <buck.ryan2@gmail.com>
2017-09-10 19:46:07 -0400

    svn file formatting

--- powerline_shell/segments/svn.py
+++ powerline_shell/segments/svn.py
@@ -2,39 +2,21 @@ import subprocess
 from ..utils import BasicSegment
 
 
-def _add_svn_segment(powerline):
-    is_svn = subprocess.Popen(['svn', 'status'],
-                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    is_svn_output = is_svn.communicate()[1].decode("utf-8").strip()
-    if len(is_svn_output) != 0:
-        return
-
-    #"svn status | grep -c "^[ACDIMRX\\!\\~]"
-    p1 = subprocess.Popen(['svn', 'status'], stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-    p2 = subprocess.Popen(['grep', '-c', '^[ACDIMR\\!\\~]'],
-            stdin=p1.stdout, stdout=subprocess.PIPE)
-    output = p2.communicate()[0].decode("utf-8").strip()
-    if len(output) > 0 and int(output) > 0:
-        changes = output.strip()
-        powerline.append(' %s ' % changes, powerline.theme.SVN_CHANGES_FG, powerline.theme.SVN_CHANGES_BG)
-
-
 class Segment(BasicSegment):
     def add_to_powerline(self):
-        """Wraps _add_svn_segment in exception handling."""
         powerline = self.powerline
-
-        # FIXME This function was added when introducing a testing framework,
-        # during which the 'powerline' object was passed into the
-        # `add_[segment]_segment` functions instead of being a global variable. At
-        # that time it was unclear whether the below exceptions could actually be
-        # thrown. It would be preferable to find out whether they ever will. If so,
-        # write a comment explaining when. Otherwise remove.
-
-        try:
-            _add_svn_segment(powerline)
-        except OSError:
-            pass
-        except subprocess.CalledProcessError:
-            pass
+        is_svn = subprocess.Popen(["svn", "status"],
+                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        is_svn_output = is_svn.communicate()[1].decode("utf-8").strip()
+        if len(is_svn_output) != 0:
+            return
+        p1 = subprocess.Popen(["svn", "status"], stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        p2 = subprocess.Popen(["grep", "-c", r"^[ACDIMR\!\~]"],
+                stdin=p1.stdout, stdout=subprocess.PIPE)
+        output = p2.communicate()[0].decode("utf-8").strip()
+        if len(output) > 0 and int(output) > 0:
+            changes = output.strip()
+            powerline.append(" %s " % changes,
+                             powerline.theme.SVN_CHANGES_FG,
+                             powerline.theme.SVN_CHANGES_BG)
