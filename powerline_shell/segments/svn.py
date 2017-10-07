538bd54fc7bfdc0478f368372794ab0523cf813f
Emanuel Angelo <emanuel.angelo@gmail.com>
2017-10-07 18:39:46 +0100

    Displays branch name for the svn segment
    SVN implements the branch notion at server level and the working copies don't
    hold that information. This implementation gets the working copy root path and
    extracts the basename, using it as a local branch name.

--- powerline_shell/segments/svn.py
+++ powerline_shell/segments/svn.py
@@ -13,6 +13,16 @@ def svn_subprocess_env():
     return {"PATH": get_PATH()}
 
 
+def _get_svn_branch():
+    p = subprocess.Popen(["svn", "info"],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         env=svn_subprocess_env())
+    working_dir_root_path = p.communicate()[0].decode("utf-8").splitlines()[1]
+    branch = os.path.basename(working_dir_root_path.split()[-1])
+    return branch
+
+
 def parse_svn_stats(status):
     stats = RepoStats()
     for line in status:
@@ -41,15 +51,16 @@ def build_stats():
         return None
     pdata = p.communicate()
     if p.returncode != 0 or pdata[1][:22] == b'svn: warning: W155007:':
-        return None
+        return None, None
     status = _get_svn_status(pdata)
     stats = parse_svn_stats(status)
-    return stats
+    branch = _get_svn_branch()
+    return stats, branch
 
 
 class Segment(ThreadedSegment):
     def run(self):
-        self.stats = build_stats()
+        self.stats, self.branch = build_stats()
 
     def add_to_powerline(self):
         self.join()
@@ -61,8 +72,8 @@ class Segment(ThreadedSegment):
             bg = self.powerline.theme.REPO_DIRTY_BG
             fg = self.powerline.theme.REPO_DIRTY_FG
         if self.powerline.segment_conf("vcs", "show_symbol"):
-            symbol = RepoStats().symbols["svn"]
+            symbol = RepoStats().symbols["svn"] + " "
         else:
-            symbol = "svn"
-        self.powerline.append(" " + symbol + " ", fg, bg)
+            symbol = ""
+        self.powerline.append(" " + symbol + self.branch + " ", fg, bg)
         self.stats.add_to_powerline(self.powerline)
