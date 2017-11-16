5a1c9f6d766b7ed7f7c51c74b1541d1dd4281c2c
Jack Humbert <jack.humb@gmail.com>
2017-11-16 01:20:40 -0500

    Adds MINGW condition to job count

--- powerline_shell/segments/jobs.py
+++ powerline_shell/segments/jobs.py
@@ -14,6 +14,12 @@ class Segment(ThreadedSegment):
             output = map(lambda l: int(l.split()[2].strip()),
                 output_proc.communicate()[0].decode("utf-8").splitlines()[1:])
             self.num_jobs = output.count(os.getppid()) - 1
+        elif platform.system().startswith('MINGW'):
+            # mingw ps is too
+            output_proc = subprocess.Popen(['ps', '-af'], stdout=subprocess.PIPE)
+            output = map(lambda l: int(l.split()[2].strip()),
+                output_proc.communicate()[0].decode("utf-8").splitlines()[1:])
+            self.num_jobs = list(output).count(os.getppid()) - 1
         else:
             pppid_proc = subprocess.Popen(['ps', '-p', str(os.getppid()), '-oppid='],
                                           stdout=subprocess.PIPE)
