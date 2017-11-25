5591dd3316c23f216f15092e2830a1e47749c780
Thomas Cherry <thomas.cherry@gmail.com>
2017-11-25 16:23:59 -0600

    error handaling, dealing with really long return times from du

--- powerline_shell/segments/du.py
+++ powerline_shell/segments/du.py
@@ -12,13 +12,28 @@ class Segment(ThreadedSegment):
     @author thomas.cherry@gmail.com
     '''
     def run(self):
+        self.usage = ""
+        ignore = self.powerline.segment_conf("du", "ignore", ())
+        wd = self.powerline.cwd or os.getenv('PWD')
         cmd = ['du','-sh', "."]
-        self.usage = subprocess.check_output(cmd).split()[0].decode('utf-8')
         
+        rule = "(^" + "$)|(^".join(ignore) + "$)"
+        if re.match(rule, wd):
+            return
+        
+        try:
+            nul_f = open(os.devnull, 'w') 
+            result = subprocess.check_output(cmd,stderr=nul_f,timeout=1)
+            self.usage = result.split()[0].decode('utf-8')
+        except subprocess.CalledProcessError as e:
+            self.usage = e.output.split()[0].decode('utf-8')
+        except subprocess.TimeoutExpired as e:
+            self.usage = "?"
+    
     def add_to_powerline(self):
         self.join()
         if len(self.usage) > 0:
-            text = ' (%s) ' % self.usage
+            text = '(%s)' % self.usage
             fg_color = self.powerline.theme.CWD_FG      #just use CWD's colors
             bg_color = self.powerline.theme.PATH_BG     #just use CWD's colors
             self.powerline.append(text, fg_color, bg_color)
