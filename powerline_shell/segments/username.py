1809074a237bfc3aed2ea9d7379714694fb6e811
ericLemanissier <ericLemanissier@users.noreply.github.com>
2017-12-19 09:45:10 +0100

    always using getpass package to get user

--- powerline_shell/segments/username.py
+++ powerline_shell/segments/username.py
@@ -1,10 +1,6 @@
 from ..utils import BasicSegment
 import os
-try:
-    import pwd    
-except ImportError:
-    pwd = None
-    import getpass
+import getpass
 
 
 class Segment(BasicSegment):
@@ -16,13 +12,8 @@ class Segment(BasicSegment):
             user_prompt = " %n "
         else:
             user_prompt = " %s " % os.getenv("USER")
-
-        if pwd:
-            user_name = pwd.getpwuid(os.getuid())[0]
-        else:
-            user_name = getpass.getuser()
     
-        if user_name == "root":
+        if getpass.getuser() == "root":
             bgcolor = powerline.theme.USERNAME_ROOT_BG
         else:
             bgcolor = powerline.theme.USERNAME_BG
