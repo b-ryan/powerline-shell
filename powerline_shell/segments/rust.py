012a1f3792f4e97e99fa0dac5bfaf8631f62c64a
Shing Tak Lam <shingtaklam1324@gmail.com>
2018-02-13 22:51:23 +0800

    added rust

--- powerline_shell/segments/rust.py
+++ powerline_shell/segments/rust.py
@@ -7,11 +7,17 @@ class Segment(BasicSegment):
     def add_to_powerline(self):
         powerline = self.powerline
         try:
-            p1 = subprocess.run(
-                ["rustup", "toolchain", "list"], stdout=subprocess.PIPE)
-            v = p1.stdout.decode("utf-8")
-            for line in v:
-                if "(default)" in line:
-                    powerline.append(line.split("-"), 15, 1)
+            c1 = subprocess.Popen(["ls"], stdout=subprocess.PIPE)
+            c2 = subprocess.Popen(["grep", "Cargo.toml"],
+                                  stdin=c1.stdout, stdout=subprocess.PIPE)
+            has_cargo = c2.communicate()[0].decode("utf-8")
+            if len(has_cargo) > 0:
+                p1 = subprocess.Popen(
+                    ["rustup", "toolchain", "list"], stdout=subprocess.PIPE)
+                v = p1.communicate()[0].decode("utf-8")
+                for line in v.split("\n"):
+                    if u"(default)" in line:
+                        powerline.append(line.split("-")[0], 15, 1)
+                        return
         except OSError:
             return
