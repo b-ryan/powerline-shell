25463545f38a65c77d5770f58b541c780e32ca72
David Wu <david10608@gmail.com>
2018-03-02 16:31:46 +0000

    Add withHost option

--- powerline_shell/segments/username.py
+++ powerline_shell/segments/username.py
@@ -1,21 +1,28 @@
 from ..utils import BasicSegment
 import os
 import getpass
+from socket import gethostname
 
 
 class Segment(BasicSegment):
     def add_to_powerline(self):
         powerline = self.powerline
-        if powerline.args.shell == "bash":
-            user_prompt = r" \u "
-        elif powerline.args.shell == "zsh":
-            user_prompt = " %n "
+        
+        if powerline.segment_conf("username", "withHost"):
+            hostname = gethostname()
+            user_prompt = " %s@%s " % (os.getenv("USER"),
+                    hostname.split(".")[0])
         else:
-            user_prompt = " %s " % os.getenv("USER")
+            if powerline.args.shell == "bash":
+                user_prompt = r" \u "
+            elif powerline.args.shell == "zsh":
+                user_prompt = " %n "
+            else:
+                user_prompt = " %s " % os.getenv("USER")
 
         if getpass.getuser() == "root":
-            bgcolor = powerline.theme.USERNAME_ROOT_BG
+                bgcolor = powerline.theme.USERNAME_ROOT_BG
         else:
-            bgcolor = powerline.theme.USERNAME_BG
+                bgcolor = powerline.theme.USERNAME_BG           
 
         powerline.append(user_prompt, powerline.theme.USERNAME_FG, bgcolor)
