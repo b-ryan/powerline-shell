f0373f73163869af44392684c1ac58457d54f0b0
danny <daniel@stratisiot.com>
2018-05-14 15:38:36 -0400

    Added option to customize root segment.
    "indicator" changes the indicator to whatever you set it too. Ignores if it's blank or you're logged in as root.

--- powerline_shell/segments/root.py
+++ powerline_shell/segments/root.py
@@ -1,4 +1,5 @@
 from ..utils import BasicSegment
+from os import geteuid
 
 
 class Segment(BasicSegment):
@@ -10,9 +11,13 @@ class Segment(BasicSegment):
             'zsh': ' %# ',
             'bare': ' $ ',
         }
+        if powerline.segment_conf("root", "indicator") and geteuid():
+            indicator = powerline.segment_conf("root", "indicator")
+        else:
+            indicator = root_indicators[powerline.args.shell]
         bg = powerline.theme.CMD_PASSED_BG
         fg = powerline.theme.CMD_PASSED_FG
         if powerline.args.prev_error != 0:
             fg = powerline.theme.CMD_FAILED_FG
             bg = powerline.theme.CMD_FAILED_BG
-        powerline.append(root_indicators[powerline.args.shell], fg, bg, sanitize=False)
+        powerline.append(indicator, fg, bg, sanitize=False)
