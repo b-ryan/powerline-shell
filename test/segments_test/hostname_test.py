0c8cdd8cdc3b00987b9cd0d21335458c88b2f3bd
David Cachau <cachaudavid@gmail.com>
2018-08-14 11:35:30 +0200

    Update test for hostname
    use side_effect instead of return_value as fg and bg are string and not boolean
    C.f. build 63 on Travis

--- test/segments_test/hostname_test.py
+++ test/segments_test/hostname_test.py
@@ -3,16 +3,20 @@ import mock
 import powerline_shell.segments.hostname as hostname
 from powerline_shell.themes.default import Color
 from argparse import Namespace
-
+from ..testing_utils import dict_side_effect_fn
 
 class HostnameTest(unittest.TestCase):
     def setUp(self):
         self.powerline = mock.MagicMock()
         self.powerline.theme = Color
+        self.powerline.segment_conf.side_effect = dict_side_effect_fn({
+            ("hostname", "colorize"): True,
+            ("hostname", "fg", ""): "",
+            ("hostname", "bg", ""): "",
+        })
         self.segment = hostname.Segment(self.powerline)
 
     def test_colorize(self):
-        self.powerline.segment_conf.return_value = True
         self.segment.start()
         self.segment.add_to_powerline()
         args = self.powerline.append.call_args[0]
