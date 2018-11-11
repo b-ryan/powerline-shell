d73fc29c4d300ca140f0a0a3109934a86000bcb1
Michael Suelmann <suelmann@gmx.de>
2018-11-11 12:38:43 +0100

    Added missing import

--- powerline_shell/segments/git.py
+++ powerline_shell/segments/git.py
@@ -1,3 +1,4 @@
+import os
 import re
 import subprocess
 from ..utils import RepoStats, ThreadedSegment, get_git_subprocess_env
--- powerline_shell/segments/hg.py
+++ powerline_shell/segments/hg.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from ..utils import RepoStats, ThreadedSegment, get_subprocess_env
 
