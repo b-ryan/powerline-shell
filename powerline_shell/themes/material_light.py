38153373feaf872e6ffdbaa095c19dccac215480
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-01-10 14:16:54 -0800

    Add custom Material light theme

---
+++ powerline_shell/themes/material_light.py
@@ -0,0 +1,65 @@
+from powerline_shell.themes.default import DefaultColor
+
+
+class Color(DefaultColor):
+    HOME_SPECIAL_DISPLAY = True
+    HOME_BG = 15
+    HOME_FG = 31
+    PATH_BG = 15
+    PATH_FG = 254
+    CWD_FG = 8
+    SEPARATOR_FG = 251
+
+    READONLY_BG = 124
+    READONLY_FG = 254
+
+    SSH_BG = 166  # medium orange
+    SSH_FG = 254
+
+    REPO_CLEAN_BG = 150  # pale green
+    REPO_CLEAN_FG = 235
+    REPO_DIRTY_BG = 203  # pale red
+    REPO_DIRTY_FG = 15
+
+    JOBS_BG = 39
+    JOBS_FG = 15
+
+    CMD_PASSED_BG = 236
+    CMD_PASSED_FG = 15
+    CMD_FAILED_BG = 161
+    CMD_FAILED_FG = 15
+
+    SVN_CHANGES_BG = 148
+    SVN_CHANGES_FG = 22  # dark green
+
+    GIT_AHEAD_BG = 240
+    GIT_AHEAD_FG = 250
+    GIT_BEHIND_BG = 240
+    GIT_BEHIND_FG = 250
+    GIT_STAGED_BG = 22
+    GIT_STAGED_FG = 15
+    GIT_NOTSTAGED_BG = 130
+    GIT_NOTSTAGED_FG = 15
+    GIT_UNTRACKED_BG = 15
+    GIT_UNTRACKED_FG = 52
+    GIT_CONFLICTED_BG = 9
+    GIT_CONFLICTED_FG = 15
+    GIT_STASH_BG = 221
+    GIT_STASH_FG = 0
+
+    VIRTUAL_ENV_BG = 35  # a mid-tone green
+    VIRTUAL_ENV_FG = 00
+
+    BATTERY_NORMAL_BG = 22
+    BATTERY_NORMAL_FG = 7
+    BATTERY_LOW_BG = 196
+    BATTERY_LOW_FG = 7
+
+    AWS_PROFILE_FG = 39
+    AWS_PROFILE_BG = 238
+
+    TIME_FG = 250
+    TIME_BG = 238
+
+    RUBY_FG = 15
+    RUBY_BG = 124
