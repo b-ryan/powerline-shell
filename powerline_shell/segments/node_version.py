b0e3bdcc9b123fe9d2f99494234b14db69e7f3a8
Michael J. Giarlo <mjgiarlo@stanford.edu>
2019-06-25 09:48:25 -0700

    split ruby and node segments apart

--- powerline_shell/segments/node_version.py
+++ powerline_shell/segments/node_version.py
@@ -1,18 +1,31 @@
+import os
 import subprocess
 from ..utils import ThreadedSegment
+
+FANCY_NODE = u'\ue718'
 
 
 class Segment(ThreadedSegment):
     def run(self):
         try:
             p1 = subprocess.Popen(["node", "--version"], stdout=subprocess.PIPE)
-            self.version = p1.communicate()[0].decode("utf-8").rstrip()
+            p2 = subprocess.Popen(['sed', 's/^v//'],
+                                  stdin=p1.stdout,
+                                  stdout=subprocess.PIPE)
+            self.version = p2.communicate()[0].decode("utf-8").rstrip()
         except OSError:
             self.version = None
 
     def add_to_powerline(self):
+        # Do not render node version segment unless .ruby-version file exists
+        if not os.path.isfile('.node-version'):
+            return
+
         self.join()
         if not self.version:
             return
-        # FIXME no hard-coded colors
-        self.powerline.append("node " + self.version, 15, 18)
+
+        if self.powerline.segment_conf('node_version', 'mode') == 'fancy':
+            self.version = u'{} {}'.format(FANCY_NODE, self.version)
+
+        self.powerline.append(' %s ' % self.version, self.powerline.theme.VIRTUAL_ENV_FG, self.powerline.theme.VIRTUAL_ENV_BG)
--- powerline_shell/segments/ruby_version.py
+++ powerline_shell/segments/ruby_version.py
@@ -1,59 +1,36 @@
 import os
 import subprocess
-from ..utils import BasicSegment
+from ..utils import ThreadedSegment
 
 FANCY_RUBY = u'\ue23e'
-FANCY_NODE = u'\ue718'
-
-# SYSTEM_NODE_VERSION = '7.10.0'
-# SYSTEM_RUBY_VERSION = 'ruby 2.5.3p105'
 
 
-def _get_node_version():
-    try:
-        p1 = subprocess.Popen(['node', '-v'], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(['sed', 's/^v//'],
-                              stdin=p1.stdout,
-                              stdout=subprocess.PIPE)
-        return p2.communicate()[0].decode('utf-8').rstrip()
-    except OSError:
-        raise
-
-
-def _get_ruby_version():
-    try:
-        p1 = subprocess.Popen(['ruby', '-v'], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(['sed', 's/ (.*//'],
-                              stdin=p1.stdout,
-                              stdout=subprocess.PIPE)
-        return p2.communicate()[0].decode('utf-8').rstrip()
-    except OSError:
-        raise
-
-
-class Segment(BasicSegment):
-    def add_to_powerline(self):
-        powerline = self.powerline
-
+class Segment(ThreadedSegment):
+    def run(self):
         try:
-            ruby_version = _get_ruby_version()
-            node_version = _get_node_version()
+            p1 = subprocess.Popen(['ruby', '-v'], stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(['sed', 's/ (.*//'],
+                                  stdin=p1.stdout,
+                                  stdout=subprocess.PIPE)
+            self.version = p2.communicate()[0].decode('utf-8').rstrip()
         except OSError:
+            self.version = None
+
+    def add_to_powerline(self):
+        # Do not render ruby version segment unless .ruby-version file exists
+        if not os.path.isfile('.ruby-version'):
             return
 
-        # if ruby_version == SYSTEM_RUBY_VERSION:
-        #     ruby_version = '%s ' % FANCY_RUBY
-        if powerline.segment_conf('ruby_version', 'mode') == 'fancy':
-            ruby_version = ruby_version.replace('ruby', FANCY_RUBY)
-        # else:
-        #     pass
+        self.join()
+        if not self.version:
+            return
+
+        if self.powerline.segment_conf('ruby_version', 'mode') == 'fancy':
+            self.version = self.version.replace('ruby', FANCY_RUBY)
 
         gem_set = os.environ.get('GEM_HOME', '').split('@')[1:]
         if gem_set:
-            ruby_version += "@{}".format(gem_set.pop())
-        if ruby_version:
-            powerline.append(' %s ' % ruby_version, powerline.theme.RUBY_FG, powerline.theme.RUBY_BG)
+            self.version += "@{}".format(gem_set.pop())
 
-        # if node_version != SYSTEM_NODE_VERSION:
-        node_string = u'{} {}'.format(FANCY_NODE, node_version)
-        powerline.append(' %s ' % node_string, powerline.theme.VIRTUAL_ENV_FG, powerline.theme.VIRTUAL_ENV_BG)
+        if self.version:
+            self.powerline.append(' %s ' % self.version, self.powerline.theme.RUBY_FG, self.powerline.theme.RUBY_BG)
