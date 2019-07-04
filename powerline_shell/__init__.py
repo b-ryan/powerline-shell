33d1ae1ae9a147ecfbd4f33d3b346e4f4767a027
Thomas Cherry <thomas.cherry@gmail.com>
2019-07-03 23:23:08 -0400

    removing need to explicity call out fish from the command line

--- powerline_shell/__init__.py
+++ powerline_shell/__init__.py
@@ -160,7 +160,7 @@ def main():
                             help='Generate the default config and print it to stdout')
     arg_parser.add_argument('--shell', action='store', default='bash',
                             help='Set this to your shell type',
-                            choices=['bash', "fish", 'tcsh', 'zsh', 'bare'])
+                            choices=['bash', 'tcsh', 'zsh', 'bare'])
     arg_parser.add_argument('prev_error', nargs='?', type=int, default=0,
                             help='Error code returned by the last command')
     args = arg_parser.parse_args()
