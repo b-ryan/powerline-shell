9e57afb623372f87312b2b44e7fcf205f9ec32b1
Thomas Cherry <thomas.cherry@gmail.com>
2019-07-23 21:37:15 -0400

    #468 - adding a check for env and providing a default if it does not exist

--- powerline_shell/segments/env.py
+++ powerline_shell/segments/env.py
@@ -4,7 +4,11 @@ from ..utils import BasicSegment
 
 class Segment(BasicSegment):
     def add_to_powerline(self):
-        self.powerline.append(
-            " %s " % os.getenv(self.segment_def["var"]),
-            self.segment_def.get("fg_color", self.powerline.theme.PATH_FG),
-            self.segment_def.get("bg_color", self.powerline.theme.PATH_BG))
+        env = os.getenv(self.segment_def["var"])
+        if env is None:
+            env = self.segment_def["default"]
+        if env is not None:
+            self.powerline.append(
+                " %s " % env,
+                self.segment_def.get("fg_color", self.powerline.theme.PATH_FG),
+                self.segment_def.get("bg_color", self.powerline.theme.PATH_BG))
