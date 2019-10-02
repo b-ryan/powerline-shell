61f115684161a3d66999139e5580e7bb3f2bea77
Wiktor Gonczaronek <w.gonczaronek@merixstudio.com>
2019-10-02 11:21:14 +0200

    Add custom indicator in root
    Users can define custom indicators in root segment.

--- powerline_shell/segments/root.py
+++ powerline_shell/segments/root.py
@@ -4,15 +4,20 @@ from ..utils import BasicSegment
 class Segment(BasicSegment):
     def add_to_powerline(self):
         powerline = self.powerline
+        indicator = self._get_indicator(powerline.args.shell, powerline)
+        bg = powerline.theme.CMD_PASSED_BG
+        fg = powerline.theme.CMD_PASSED_FG
+        if powerline.args.prev_error != 0:
+            fg = powerline.theme.CMD_FAILED_FG
+            bg = powerline.theme.CMD_FAILED_BG
+        powerline.append(indicator, fg, bg, sanitize=False)
+
+    def _get_indicator(self, shell, powerline):
+        custom_indicator = powerline.segment_conf("root", "indicator", None)
         root_indicators = {
             'bash': ' \\$ ',
             'tcsh': ' %# ',
             'zsh': ' %# ',
             'bare': ' $ ',
         }
-        bg = powerline.theme.CMD_PASSED_BG
-        fg = powerline.theme.CMD_PASSED_FG
-        if powerline.args.prev_error != 0:
-            fg = powerline.theme.CMD_FAILED_FG
-            bg = powerline.theme.CMD_FAILED_BG
-        powerline.append(root_indicators[powerline.args.shell], fg, bg, sanitize=False)
+        return custom_indicator or root_indicators[shell]
