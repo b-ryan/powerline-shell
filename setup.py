0b7f6b93a69934c3e40f098bc10febfe40246a41
Daniel Milnes <daniel@daniel-milnes.uk>
2023-10-01 19:09:00 +0100

    Remove Explicit Dependancy on Argparse
    Argparse has been part of the Python stdlib since 2.7 and 3.2, so there is no need to explicitly depend on it.

--- setup.py
+++ setup.py
@@ -22,9 +22,6 @@ setup(
         "powerline_shell.segments",
         "powerline_shell.themes",
     ],
-    install_requires=[
-        "argparse",
-    ],
     entry_points="""
     [console_scripts]
     powerline-shell=powerline_shell:main
